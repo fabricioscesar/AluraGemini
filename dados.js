776de63217bba9af05e7c0e2f1bbd17f30725fad
fabricioscesar <90213175+fabricioscesar@users.noreply.github.com>
2024-09-09 22:37:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Kotlin",
+        descricao : "Kotlin é uma linguagem de programação moderna e concisa, ideal para quem busca produtividade e segurança no desenvolvimento de software. Criada pela JetBrains, ela é amplamente utilizada no desenvolvimento Android, mas também pode ser aplicada em outras plataformas como web, desktop e backend.",
+        link : "https://pt.wikipedia.org/wiki/Kotlin",
+        tags : "android jetbrains multiplataforma null safety corrotinas"
+    },
+    {
+        titulo : "Python",
+        descricao : "Python é uma linguagem de programação de alto nível, interpretada, de propósito geral e de código aberto. Possui uma filosofia de projeto que enfatiza a legibilidade do código, com o uso de indentação significativa.",
+        link : "https://pt.wikipedia.org/wiki/Python",
+        tags : "data science machine learning deep learning cientifico"
+    },
+    {
+        titulo : "JavaScript",
+        descricao : "JavaScript é uma linguagem de programação interpretada, multi-paradigma, de fraco tipagem, mais conhecida como a linguagem dos navegadores, utilizada para criar interatividade em páginas da Web.",
+        link : "https://pt.wikipedia.org/wiki/JavaScript",
+        tags : "frontend back-end node.js react angular"
+    }
+];
